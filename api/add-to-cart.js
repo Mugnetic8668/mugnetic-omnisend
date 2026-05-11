337666707d949a94b741b466a80a7d5fffad0871
thaikt123 <136434116+thaikt123@users.noreply.github.com>
2026-05-11 11:47:36 +0700

    fix cors

--- api/add-to-cart.js
+++ api/add-to-cart.js
@@ -2,6 +2,28 @@ import axios from "axios";
 
 export default async function handler(req, res) {
 
+  // CORS
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  // Handle preflight
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  
   if (req.method !== "POST") {
     return res.status(405).json({
       message: "Method not allowed"
